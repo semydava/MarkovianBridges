857e2194f74f1ae5d8b0e5f94a898d368aa60b02
bohdanp44 <bogdan.p4444@gmail.com>
2022-10-06 13:15:29 +0200

    add exp convergence

--- test.py
+++ test.py
@@ -1,6 +1,9 @@
+#from cmath import sqrt
+from imaplib import Internaldate2tuple
 import random
 import numpy as np
-import sdepy
+import math
+from math import sqrt
 import matplotlib.pyplot as plt
 from decimal import *
 def discrete_noise():
@@ -10,13 +13,10 @@ def discrete_noise():
     elif prob > 0.5:
         y = -1
     return y
-def AR(T, a, b):
-    theta = 0.9
-    mu = 0
-    sigma = 1
+def AR(T, a, b, theta, sigma):
     dt = 0.001
     n = int(T/dt)
-    print(n)
+    #print(n)
     t = np.linspace(0, T, n)
     #dt = T/n
     X1 = np.zeros(t.shape)
@@ -28,16 +28,13 @@ def AR(T, a, b):
 
     for i in range(t.size - 1):
         n1 = discrete_noise()
-        #n2 = discrete_noise()
-        X1[i + 1] = X1[i] + mu * (theta - X1[i]) * dt + sigma * np.sqrt(dt) * n1
-        X2[i + 1] = X2[i] + mu * (theta - X2[i]) * dt + sigma * np.sqrt(dt) * n1
+        n2 = discrete_noise()
+        X1[i + 1] = X1[i] - theta * X1[i] * dt + sigma * np.sqrt(dt) * n1
+        X2[i + 1] = X2[i] - theta * X2[i] * dt + sigma * np.sqrt(dt) * n2
     X2 = X2[::-1]
     X1 = list(X1)
     X2 = list(X2)
     for index, (x1, x2) in enumerate(zip(X1, X2)):
-        #print(x1, x2)
-        #index1 = np.where(X1 == x1)[0][0]
-        #print(index)
         if index < len(X1) - 1:
             new_index1 = index + 1
             n_x1 = X1[new_index1]
@@ -65,93 +62,200 @@ def AR(T, a, b):
             else:
                 z2[idx] = X2[idx]
                 t2.append(t[idx])
-        # stop_time = t[stop_index]
-        # for i in t:
-        #     if i <= stop_time:
-        #         t1.append(i)
-        #     else:
-        #         t2.append(i)
-        fig, ax = plt.subplots(2)
-        #ax[0].set_ylim(-1, 3)
-        #ax[1].set_ylim(-1, 3)
-        ax[0].plot(t, X1, label="Process Xˆ1 which starts at a = - 0.5", color="blue")
-        #ax[0].scatter(t, X1, label="Process Xˆ1 which starts at a = - 0.5", color="blue")
-        ax[0].plot(t, X2, label="Process Xˆ2 which starts at b = 1", color="orange")
-        #ax[0].scatter(t, X2, label="Process Xˆ2 which starts at b = 1", color="orange")
-        ax[0].plot(t[stop_index], X1[stop_index], 'ro', label= "first time processes intersect")
-        ax[0].legend(loc='upper center', bbox_to_anchor=(0.5, 1.25), ncol=3, fancybox=True, shadow=True)
-        ax[1].plot(t1, z1.values(), color="blue")
-        ax[1].plot(t2, z2.values(), color="orange")
-        ax[1].plot(t[stop_index], X1[stop_index], 'ro', label="first time processes intersect")
-        ax[1].legend(loc='upper left')
-        #plt.plot(t, X1)
-        #plt.plot(t[stop_index], X1[stop_index], 'ro')
-        #plt.plot(t, X2)
-        # plt.grid(True)
-        # plt.xlabel('t')
-        # plt.ylabel('X')
+        
+
+        
+        #stop_time = t[stop_index]
+       
+        #fig, ax = plt.subplots(2)
+        # #ax[0].set_ylim(-1, 3)
+        # #ax[1].set_ylim(-1, 3)
+        #ax[0].plot(t, X1, label="Process Xˆ1 which starts at a = - 0.5", color="blue")
+        #ax[0].plot(t, X2, label="Process Xˆ2 which starts at b = 1", color="orange")
+        #ax[0].plot(t[stop_index], X1[stop_index], 'ro', label= "first time processes intersect")
+        # ax[0].legend(loc='upper center', bbox_to_anchor=(0.5, 1.25), ncol=3, fancybox=True, shadow=True)
+        #ax[1].plot(t1, z1.values(), color="blue")
+        #ax[1].plot(t2, z2.values(), color="orange")
+        #ax[1].plot(t[stop_index], X1[stop_index], 'ro', label="first time processes intersect")
+        # ax[1].legend(loc='upper left')
+        # #plt.plot(t, X1)
+        # #plt.plot(t[stop_index], X1[stop_index], 'ro')
+        # #plt.plot(t, X2)
+        # # plt.grid(True)
+        # # plt.xlabel('t')
+        # # plt.ylabel('X')
         #plt.show()
     return intersect
-#print(AR(1., 0, 0.5))
+#print(AR(1., 0, 0.5, 0.9, 1))
 
-def Monte_Carlo_b1(sim_n,T):
-    count = 0
-    list1 = []
+def Monte_Carlo_sigma(sim_n,sigma):
+    count1 = 0
+    count2 = 0
+    count3 = 0
     for i in range(sim_n):
-        inter = AR(T, 0, 0.5)
-        count += inter
-    prob_inter = (count / sim_n)
-    return prob_inter
-#print(Monte_Carlo_b1(10, 50))
+        inter1 = AR(5, -0.5, 0.9, 0.2, sigma)
+        inter2 = AR(5, -0.5, 0.9, 0.5, sigma)
+        inter3 = AR(5, -0.5, 0.5, 0.9, sigma)
+        count1 += inter1
+        count2 += inter2
+        count3 += inter3
 
-def Monte_Carlo_b2(sim_n,T):
-    count = 0
-    list1 = []
-    for i in range(sim_n):
-        inter = AR(T, -0.5, 0.5)
-        count += inter
-    prob_inter = (count / sim_n)
-    return prob_inter
-#print(Monte_Carlo_b2(10, 30000))
-def Monte_Carlo_b3(sim_n,T):
-    count = 0
-    list1 = []
-    for i in range(sim_n):
-        inter = AR(T, -0.5, 1)
-        count += inter
-    prob_inter = (count / sim_n)
-    return prob_inter
+    prob_inter1 = (count1 / sim_n)
+    prob_inter2 = (count2 / sim_n)
+    prob_inter3 = (count3 / sim_n)
 
-def prob_plot():
-    #n_s = [i for i in (range(1, n+1))]
-    #T_s = [i*0.01 for i in n_s]
-    #T_s = [0.001, 0.0011, 0.0012, 0.0013, 0.0014, 0.0015, 0.0016,  0.0017,  0.0018, 0.0019, 0.002, 0.0021, 0.0022, 0.0023, 0.0024, 0.0025, 0.0026, 0.0027, 0.0028, 0.0029, 0.003, 0.0031, 0.0032, 0.0033, 0.0034, 0.0035, 0.0036, 0.0037, 0.0038, 0.0039, 0.004, 0.0041, 0.0042, 0.0043, 0.0044, 0.0045, 0.0046, 0.0047, 0.0048, 0.0049, 0.005, 0.0051, 0.0052, 0.0053, 0.0054, 0.0055, 0.0056, 0.0057, 0.0058, 0.0059, 0.006, 0.0061, 0.0062, 0.0063, 0.0064, 0.0065, 0.0066, 0.0067, 0.0068, 0.0069, 0.007, 0.0071, 0.0072, 0.0073, 0.0074, 0.0075, 0.0076, 0.0077, 0.0078, 0.0079, 0.008, 0.0081, 0.0082, 0.0083, 0.0084, 0.0085, 0.0086, 0.0087, 0.0028, 0.0089, 0.009, 0.0091, 0.0092, 0.0093, 0.0094, 0.0095, 0.0096, 0.0097, 0.0098, 0.0099, 0.01, 0.011, 0.012, 0.013, 0.014, 0.015, 0.016,  0.017,  0.018, 0.019, 0.02, 0.021, 0.022, 0.023, 0.024, 0.025, 0.026, 0.027, 0.028, 0.029, 0.03, 0.031, 0.032, 0.033, 0.034, 0.035, 0.036, 0.037, 0.038, 0.039, 0.04, 0.041, 0.042, 0.043, 0.044, 0.045, 0.046, 0.047, 0.048, 0.049, 0.05, 0.06, 0.07, 0.08, 0.09, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0, 5.0, 10.0]
-    #n_s = [i for i in range(1, T+1)]
-    #n_s = [int(i/0.001) for i in range(1, T+1)]
-    #n_s = [int(i/0.001) for i in range(1, T+1)]
+    return [prob_inter1, prob_inter2, prob_inter3]  
+
+
+def Monte_Carlo(sim_n,T, theta, sigma):
+    count1 = 0
+    count2 = 0
+    count3 = 0
+
+    for i in range(sim_n):
+        inter1 = AR(T, 0, 0.5, theta, sigma)
+        inter2 = AR(T, -0.5, 0.5, theta, sigma)
+        inter3 = AR(T, -0.5, 1, theta, sigma)
+        count1 += inter1
+        count2 += inter2
+        count3 += inter3
+   
+
+    prob_inter1 = (count1 / sim_n)
+    prob_inter2 = (count2 / sim_n)
+    prob_inter3 = (count3 / sim_n)
+
+    return [prob_inter1, prob_inter2, prob_inter3] 
+
+def prob_plot_coef():
     prob_freq1 = []
     prob_freq2 = []
     prob_freq3 = []
     prob_b = []
     i = 0
-    T_s = []
-    while i <= 5.:
+    sigmas = []
+    while i <= 10.:
         i += 0.1
+        sigmas.append(i)
+    for i in sigmas:
+        print('l')
+        p = Monte_Carlo_sigma(50, i)
+        prob_freq1.append(p[0])
+        prob_freq2.append(p[1])
+        prob_freq3.append(p[2])
+    plt.title("Probability of stopping time detection")
+    plt.xlabel('Different sigma coefficients')
+    plt.ylabel('Probability')
+    plt.plot(sigmas, prob_freq1, color="orange", label="Probability of stopping time detection with a = 0 and b = 0.5")
+    plt.plot(sigmas, prob_freq2, color="lawngreen", label="Probability of stopping time detection with a = -0.5 and b = 0.5")
+    plt.plot(sigmas, prob_freq3, color="purple", label="Probability of stopping time detection with a = -0.5 and b = 1")
+    plt.legend(loc='lower right')
+    plt.show()
+ 
+#print(prob_plot_coef())
+
+def distance(x1,y1,x2,y2):
+     e1 = (x2 - x1)**2 + (y2 - y1)**2
+     return sqrt(e1)
+
+def dist_avr(T):
+    dis1 = distance(0, 0, 0.5,T)
+    dis2 = distance(0, -0.5, 0.5,T)
+    dis3 = distance(0, -0.5, 1,T)
+    avrg = (dis1 + dis2 + dis3)/3
+        
+    return avrg
+
+
+
+
+def prob_plot_time(T, theta, sigma):
+    prob_freq1 = []
+    prob_freq2 = []
+    prob_freq3 = []
+    prob_e = []
+    i = 0
+    T_s = []
+    while i <= T:
+        i += 0.05
         T_s.append(i)
     for i in T_s:
+        lam = dist_avr(i)
+        e = 1 - math.exp(- lam * i)
+        #e = 1 - math.exp(- theta * i)*sigma**2/theta*2
+        prob_e.append(e)
         print('l')
-        p1 = Monte_Carlo_b1(20, i)
-        p2 = Monte_Carlo_b2(20, i)
-        p3 = Monte_Carlo_b3(20, i)
-        prob_freq1.append(p1)
-        prob_freq2.append(p2)
-        prob_freq3.append(p3)
+        p = Monte_Carlo(10, i, theta, sigma)
+        prob_freq1.append(p[0])
+        prob_freq2.append(p[1])
+        prob_freq3.append(p[2])
     plt.title("Probability of stopping time detection")
     plt.xlabel('Different time Δ')
     plt.ylabel('Probability')
-    plt.plot(T_s, prob_freq1, color="orange", label="Probability of stopping time detection with a = 0 and b = 0.5")
-    plt.plot(T_s, prob_freq2, color="lawngreen", label="Probability of stopping time detection with a = -0.5 and b = 0.5")
-    plt.plot(T_s, prob_freq3, color="purple", label="Probability of stopping time detection with a = -0.5 and b = 1")
-    plt.legend(loc='lower right')
+    plt.plot(T_s, prob_freq1, color="orange", label="Probability of stopping time detection with a1 = 0, b1 = 0.5, θ = " + str(theta) + " and σ = " + str(sigma))
+    plt.plot(T_s, prob_freq2, color="lawngreen", label="Probability of stopping time detection with a2 = -0.5, b2 = 0.5,  θ = " + str(theta) + " and σ = " + str(sigma))
+    plt.plot(T_s, prob_freq3, color="purple", label="Probability of stopping time detection with a3 = -0.5, b3 = 1, θ = " + str(theta) + " and σ = " + str(sigma))
+    plt.plot(T_s, prob_e, color="black", label="f(x) = 1 - exp(- λ*Δ)")
+    plt.legend(loc=(0.5, 0.01))
     plt.show()
-print(prob_plot())
+print(prob_plot_time(5, 0.5, 0.5))
+
+def prob_plot_aprox():
+    prob_freq1 = []
+    prob_freq2 = []
+    prob_freq3 = []
+    prob_freq4 = []
+    prob_freq5 = []
+    prob_freq6 = []
+    prob_freq7 = []
+    prob_freq8 = []
+    prob_freq9 = []
+    prob_e = []
+    i = 0
+    T_s = []
+    while i <= 10.:
+        i += 0.05
+        T_s.append(i)
+    for i in T_s:
+        lam = dist_avr(i)
+        e = 1 - np.exp(- lam * i)
+        prob_e.append(e)
+        print('l')
+        p = Monte_Carlo(300, i)
+        prob_freq1.append(p[0])
+        prob_freq2.append(p[1])
+        prob_freq3.append(p[2])
+        prob_freq4.append(p[3])
+        prob_freq5.append(p[4])
+        prob_freq6.append(p[5])
+        prob_freq7.append(p[6])
+        prob_freq8.append(p[7])
+        prob_freq9.append(p[8])
+
+    fig, ax = plt.subplots(3)
+    ax[0].set_xlabel('Different time Δ')
+    ax[0].set_ylabel('Probability')
+    ax[1].set_xlabel('Different time Δ')
+    ax[1].set_ylabel('Probability')
+    ax[2].set_xlabel('Different time Δ')
+    ax[2].set_ylabel('Probability')
+    ax[0].plot(T_s, prob_e, color="black", label="f(x) = 1 - exp(- λ*Δ)")
+    ax[0].plot(T_s, prob_freq1, color="orange", label="Probability of stopping time detection with a1 = 0 and b1 = 0.5, theta = 1, sigma = 1")
+    ax[0].plot(T_s, prob_freq2, color="lawngreen", label="Probability of stopping time detection with a2 = -0.5 and b2 = 0.5, theta = 1, sigma = 1")
+    ax[0].plot(T_s, prob_freq3, color="purple", label="Probability of stopping time detection with a3 = -0.5 and b3 = 1, theta = 1, sigma = 1")
+    ax[1].plot(T_s, prob_e, color="black", label="f(x) = 1 - exp(- λ*Δ)")
+    ax[1].plot(T_s, prob_freq4, color="orange", label="Probability of stopping time detection with a1 = 0 and b1 = 0.5, theta = 0.7, sigma = 2.5")
+    ax[1].plot(T_s, prob_freq5, color="lawngreen", label="Probability of stopping time detection with a2 = -0.5 and b2 = 0.5, theta = 0.7, sigma = 2.5")
+    ax[1].plot(T_s, prob_freq6, color="purple", label="Probability of stopping time detection with a3 = -0.5 and b3 = 1, theta = 0.7, sigma = 2.5")
+    ax[2].plot(T_s, prob_e, color="black", label="f(x) = 1 - exp(- λ*Δ)")
+    ax[2].plot(T_s, prob_freq7, color="orange", label="Probability of stopping time detection with a1 = 0 and b1 = 0.5, theta = 3, sigma = 0.5")
+    ax[2].plot(T_s, prob_freq8, color="lawngreen", label="Probability of stopping time detection with a2 = -0.5 and b2 = 0.5, theta = 3, sigma= 0.5")
+    ax[2].plot(T_s, prob_freq9, color="purple", label="Probability of stopping time detection with a3 = -0.5 and b3 = 1,  theta = 3, sigma = 0.5")
+    ax[0].legend(loc=(0.5, 0.01))
+    ax[1].legend(loc=(0.5, 0.01))
+    ax[2].legend(loc=(0.5, 0.01))
+    plt.show()
+
+
+
+
